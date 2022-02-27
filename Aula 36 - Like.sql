be68ea76239c4e754acc261f4425e3aacfe1a5ce
Bryan <bryanlima200@outlook.com>
2022-02-27 19:34:07 -0300

    Adicionado comandos executados na Aula 36 - Like

---
+++ Aula 36 - Like.sql
@@ -0,0 +1,17 @@
+SELECT * FROM Alunos;
+
+
+SELECT * FROM Alunos
+WHERE Nome LIKE 'Rafael';
+
+SELECT * FROM Alunos
+WHERE Nome LIKE 'Ra%';
+
+SELECT * FROM Alunos
+WHERE Nome LIKE '%o';
+
+SELECT * FROM Alunos
+WHERE Nome LIKE '%a%';
+
+SELECT * FROM Alunos
+WHERE Nome LIKE 'R%l';
