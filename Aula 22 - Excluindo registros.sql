48a3fff265da79a41003d68f8791cae54098ca0c
Bryan <bryanlima200@outlook.com>
2022-02-27 14:50:56 -0300

    Adicionado comandos executados na Aula 22 - Excluindo registros

---
+++ Aula 22 - Excluindo registros.sql
@@ -0,0 +1,15 @@
+-- DELETE FROM Alunos;
+
+-- TRUNCATE TABLE Alunos;
+
+-- DELETE TOP (1) FROM Alunos;
+
+-- DELETE TOP (10) PERCENT FROM Alunos;
+
+
+
+SELECT * FROM Alunos;
+
+DELETE TOP (1) FROM Alunos;
+
+SELECT * FROM Alunos;
