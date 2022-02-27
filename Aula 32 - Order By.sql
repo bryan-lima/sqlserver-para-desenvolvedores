23c8c735bdc53f9d4dd3004cec30932e797c59f1
Bryan <bryanlima200@outlook.com>
2022-02-27 17:54:19 -0300

    Adicionado comandos executados na Aula 32 - Order By

---
+++ Aula 32 - Order By.sql
@@ -0,0 +1,24 @@
+SELECT * FROM Alunos;
+
+
+SELECT * FROM Alunos
+ORDER BY Nome;
+
+SELECT * FROM Alunos
+ORDER BY Nome ASC;
+
+SELECT * FROM Alunos
+ORDER BY Nome DESC;
+
+
+
+SELECT * FROM Alunos
+ORDER BY 1 DESC;
+
+SELECT * FROM Alunos
+ORDER BY 1 ASC;
+
+
+
+SELECT * FROM Alunos
+ORDER BY Nome ASC, Cpf DESC, Estado ASC;
