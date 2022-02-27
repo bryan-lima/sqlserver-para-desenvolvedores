ff5b04f766939770cb9baaf96d2e732d64fa6132
Bryan <bryanlima200@outlook.com>
2022-02-27 14:34:57 -0300

    Adicionado comandos executados na Aula 19 - Inserindo registros

---
+++ Aula 19 - Inserindo registros.sql
@@ -0,0 +1,7 @@
+-- INSERT INTO Alunos (Nome, Cpf, DataNascimento, Ativo)
+-- VALUES ('Bryan', '12345678911', GETDATE(), 0);
+
+-- INSERT INTO Alunos (Nome, Cpf, DataNascimento, Ativo)
+-- VALUES 
+--     ('Eduardo', '12365478900', GETDATE(), 0), 
+--     ('Bruno', '98765432111', GETDATE(), 0);
