ad5bf356611cfe4b5009f1bc57ed63e8f757475c
Bryan <bryanlima200@outlook.com>
2022-02-27 21:16:08 -0300

    Adicionado comandos executados na Aula 45 - Left Join

---
+++ Aula 45 - Left Join.sql
@@ -0,0 +1,17 @@
+INSERT INTO Categorias (Descricao)
+VALUES ('Categoria Test');
+
+SELECT * FROM Categorias;
+
+SELECT * FROM Cursos;
+
+
+SELECT CR.Descricao Curso, CA.Descricao Categoria
+FROM Cursos CR
+INNER JOIN Categorias CA
+ON CA.Id = CR.CategoriaId;
+
+SELECT CR.Descricao Categoria, CA.Descricao Curso
+FROM Categorias CR
+LEFT JOIN Cursos CA
+ON CA.CategoriaId = CR.Id;
